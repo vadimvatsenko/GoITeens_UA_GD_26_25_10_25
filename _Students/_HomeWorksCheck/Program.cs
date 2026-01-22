6db58c6cdef1714f2ed0426a3ea34a9520522d03
Vadym <69381493+vadimvatsenko@users.noreply.github.com>
2026-01-22 20:18:01 +0200

    add last changes

--- _Students/_HomeWorksCheck/Program.cs
+++ _Students/_HomeWorksCheck/Program.cs
@@ -7,19 +7,60 @@ namespace _HomeWorksCheck
     {
         static void Main(string[] args)
         {
-            List<int> planets = new List<int>() { 150, 350 };
-            
-            for(int i= 0;i < 5 ;i++)
+            int playerEnergy;
+
+            Dictionary<int, string> dic = new Dictionary<int, string>()
             {
-                int next = planets[i + 1] + (planets[i + 1] - planets[i]) * 2 ;
-                planets.Add(next);
+                { 10, "Fire" },
+                { 40, "Ice Rain" },
+                { 5, "Snowball" },
+                { 70, "Thunder" }
+            };
+            foreach (KeyValuePair<int, string> item in dic)
+            {
+                Console.WriteLine($"Skill:{item.Key} - {item.Value}");
             }
-            Console.WriteLine(string.Join(", ", planets));
-            
+
+            Console.WriteLine("Which skill would you like to use?(write only first number)");
+            int energy = int.Parse(Console.ReadLine());
+
+            Console.Write($"Your choice:{dic[energy]}\n");
+
+            Console.WriteLine("Now write how many energy do you have");
+            playerEnergy = int.Parse(Console.ReadLine());
+
+            bool answer;
+
+            answer = calc(playerEnergy, energy);
+
+            if (answer == true)
+            {
+                Console.WriteLine("You can use this item");
+            }
+            else
+            {
+                Console.WriteLine("You can't use this item again");
+            }
+
             Console.ReadKey();
+
+        }
+
+        static bool calc(int energy, int number)
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>()
+            {
+                { 10, "Fire" },
+                { 40, "Ice Rain" },
+                { 5, "Snowball" },
+                { 70, "Thunder" }
+            };
+            bool answer = energy >= number;
+            return answer;
         }
     }
 }
+
 
     
 
