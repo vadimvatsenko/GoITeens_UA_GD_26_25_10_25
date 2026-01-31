504affd7fc83c2d3bff29e574a730260e0e0e0b0
Vadym <69381493+vadimvatsenko@users.noreply.github.com>
2026-01-31 20:50:23 +0200

    add Magic Cristal

---
+++ Lesson_11_MagicalCrystal/Lesson_11_MagicalCrystal_01/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+// Заняття 1,5 год 5хв перева.
+// Опитування по методах.
+// Магічний кристал - на одне заняття
+// На друге ООП та класи
+
+// Мета:
+// Створимо Чарівний кристал.
+// Створити інтерактивний кристал, який відповідає на запитання за допомогою різних кольорів та повідомлень,
+// наприклад, кристал може відповідати тільки, "Так", "Ні", "Можливо"
+
+// Відео по силка викачка.
+
+namespace Lesson_11_MagicalCrystal_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> answers = GetAnswers();
+            Random random = new Random();
+
+            do
+            {
+                ClearConsole();
+                Greetings();
+                
+                string question = Console.ReadLine();
+                
+                string answer = GetAnswer(answers, random);
+
+                Console.ForegroundColor = GetConsoleColor(answer);
+                Console.WriteLine($"Кристал каже {answer}");
+                
+                Console.ReadKey();
+                
+            } while (true);
+            
+        }
+
+        private static void ClearConsole()
+        {
+            Console.ResetColor();
+            Console.Clear();
+        }
+        
+        private static void Greetings()
+        {
+            Console.WriteLine("Вітаю в Чарівному Кристалі!");
+            Console.WriteLine("Задайте питання (так чи ні), а я дам відповідь");
+        }
+
+        private static string GetAnswer(List<string> answers, Random random)
+        {
+            int randomIndex = random.Next(0, answers.Count); // від 0 до 2
+            
+            string answer = answers[randomIndex]; 
+            return answer;
+        }
+        
+        private static List<string> GetAnswers()
+        {
+            List<string> answers = new List<string>()
+            {
+                "Yes", // 0
+                "No", // 1
+                "Maybe" // 2
+            };
+            return answers;
+        }
+
+        private static ConsoleColor GetConsoleColor(string answer)
+        {
+            switch (answer)
+            {
+                case "Yes":
+                    return ConsoleColor.Green;
+                case "No":
+                    return ConsoleColor.Red;
+                case "Maybe":
+                    return ConsoleColor.Yellow;
+                default:
+                    return ConsoleColor.White;
+            }
+        }
+    }
+}
---
+++ Lesson_11_MagicalCrystal/Lesson_11_MagicalCrystal_02/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Drawing;
+
+// Colorizer
+
+namespace Lesson_11_MagicalCrystal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, ConsoleColor> colors = new Dictionary<int, ConsoleColor>()
+            {
+                [1] = ConsoleColor.Red,
+                [2] = ConsoleColor.Blue,
+                [3] = ConsoleColor.Green,
+                [4] = ConsoleColor.Yellow,
+                [5] = ConsoleColor.Magenta,
+            };
+
+            bool isNumber = false;
+            bool isValidColorNumber = false;
+            
+            
+            while (!isNumber || !isValidColorNumber)
+            {
+                Console.Clear();
+                ShowDict(colors);
+                Console.WriteLine("Введіть номер кольору: ");
+                
+                string color = Console.ReadLine().ToLower();
+                
+                isNumber =  int.TryParse(color, out int numberColor);
+                isValidColorNumber = colors.ContainsKey(numberColor);
+
+                if (!isNumber || !isValidColorNumber)
+                {
+                    Console.WriteLine("Not Valid Color number");
+                    Console.ReadKey();
+                }
+            }
+
+            /*switch (color)
+            {
+                case "червоний":
+                    Console.WriteLine("Дракон");
+                    break;
+
+                case "синій":
+                    Console.WriteLine("Чарівна Фея");
+                    break;
+
+                case "зелений":
+                    Console.WriteLine("Лісовик");
+                    break;
+
+                case "жовтий":
+                    Console.WriteLine("Сонячний Ельф");
+                    break;
+
+                case "фіолетовий":
+                    Console.WriteLine("Маг Чарівник");
+                    break;
+
+                default:
+                    Console.WriteLine("Не відомий колір");
+                    break;
+            }
+            */
+        }
+
+        private static void ShowDict(Dictionary<int, ConsoleColor> colors)
+        {
+            foreach (var col in colors)
+            {
+                Console.WriteLine(col.Key + " " + col.Value);
+            }
+        }
+    }
+}
