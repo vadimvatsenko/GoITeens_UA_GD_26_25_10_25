cc650fc196670415179e799d4c4a7565c5f07148
Vadym <69381493+vadimvatsenko@users.noreply.github.com>
2026-04-05 10:48:07 +0300

    ad

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -5,27 +5,34 @@ using System;
 
 namespace Sample
 {
-
-    enum CharacterClass
-    {
-        Warrior,
-        Mage,
-        Rogue,
-    }
-
-    enum CharacterState
-    {
-        Idle,
-        Attacking,
-        Defending,
-    }
     
     internal class Program
     {
         
-        
         static void Main(string[] args)
         {
+            List<int> numbs = new List<int>
+            {
+                5, 
+                6, 
+                10, 
+                15, 
+                12 // 4
+            };
+            
+            Console.WriteLine(numbs.Count); // 5
+            
+            Console.WriteLine(numbs[numbs.Count - 1]);
+
+            foreach (var n in numbs)
+            {
+                Console.WriteLine(n);
+            }
+
+            for (int i = 0; i < numbs.Count; i++)
+            {
+                Console.WriteLine(numbs[i]);
+            }
             
         }
     }
